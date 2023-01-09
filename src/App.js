9e6774191b4acd3cad5456e3a476c25c22cbd2b7
luk-fontain <luciano_rios@gmx.de>
2023-01-09 14:32:29 +0100

    14:34

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar"
 import Booklist from "./components/Booklist"
 import AddBook from "./components/AddBook";
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BookContextProvider } from "./Context/BookContext";
 
 function App() {
